9f7cc53c3cb9b6d0b1b89cb3a264a31c5f6c7692
Esther Carrelle <esthcarelle@gmail.com>
2024-03-15 16:05:18 +0200

    Created a Main Screen composable added a button click listener to show the dialog

--- app/src/main/java/com/mine/alert/MainActivity.kt
+++ app/src/main/java/com/mine/alert/MainActivity.kt
@@ -3,7 +3,10 @@ package com.mine.alert
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -11,6 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -28,16 +32,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(name = "Hello Hello")
+                    MainScreen()
                 }
             }
         }
     }
 }
-@Composable
-fun MyAlertDialog() {
-    val shouldShowDialog = remember { mutableStateOf(true) } // 1
 
+@Composable
+fun MainScreen() {
+    val shouldShowDialog = remember { mutableStateOf(false) } // 1
+
+    if (shouldShowDialog.value) {
+        MyAlertDialog(shouldShowDialog = shouldShowDialog)
+    }
+
+    Button(
+        onClick = { shouldShowDialog.value = true },
+        modifier = Modifier.wrapContentSize()
+    ) {
+        Text(text = "Show Dialog")
+    }
+}
+
+@Composable
+fun MyAlertDialog(shouldShowDialog: MutableState<Boolean>) {
     if (shouldShowDialog.value) { // 2
         AlertDialog( // 3
             onDismissRequest = { // 4
@@ -48,7 +67,6 @@ fun MyAlertDialog() {
             text = { Text(text = "Jetpack Compose Alert Dialog") },
             confirmButton = { // 6
                 Button(
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Green),
                     onClick = {
                         shouldShowDialog.value = false
                     }
